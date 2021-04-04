caef61b3132453ab5c870d0362dfba5c534214c2
unknown <archernich@gmail.com>
2021-04-04 16:15:29 -0700

    render markdown template

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -85,6 +85,8 @@ function renderLicenseSection(data) {
   }
 };
 
+
+
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.projectTitle}
